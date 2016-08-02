76b6e6b5ec3e9ff447cb8413a26179b171711237
Paul Maxeiner <paulmaxeiner@pauls-macbook-air.local>
2016-08-02 15:22:40 -0400

    Layed Out Main Storyboard

--- KindessQuizApp/ViewController.swift
+++ KindessQuizApp/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Buttons: [UIButton]!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
