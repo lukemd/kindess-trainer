21cf4a7aa5e68f7828c97dd22fad0ad34ed9072c
Paul Maxeiner <paulmaxeiner@pauls-macbook-air.local>
2016-08-03 10:52:29 -0400

    Finished Adding Questions and Randomized Questions

--- KindessQuizApp/ViewController.swift
+++ KindessQuizApp/ViewController.swift
@@ -20,11 +20,23 @@ class ViewController: UIViewController {
     
     var Questions = [Question]()
     
+    var QNumber = Int()
+    
+    var AnswerNumber = Int()
+    
+    var Score = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        Questions = [Question(Question: "Hi! My name is Yusafi! What's your name?", Answers: ["Nice to meet you! My name is \"your name\".","It's \"your name\". Where are you from?","My name is \"your name\", I kinda like your accent.","Im \"your name\"."], Answer: 1)]
+        Questions = [
+            Question(Question: "Hi! My name is Yusafi! What's your name?", Answers: ["Nice to meet you! My name is \"your name\".","It's \"your name\". Where are you from?","My name is \"your name\", I kinda like your accent.","Im \"your name\"."], Answer: 0),
+            Question(Question: "Excuse me, do you know how to get to Amsterdam?", Answers: ["Nope, sorry.","You can ask a taxi driver. Sorry.","No, but let me check my map real quick!","Sorry, I don't know where Amsterdam is."], Answer: 2),
+            Question(Question: "Hello, do you reccomend the green salad or the lettuce sandwich?", Answers: ["Never had one, sorry.","I have never had either, but the lettuce sandwich sounds good!","I think you might like some other menu items.","I don't like lettuce or salad very much."], Answer: 1),
+            Question(Question: "Excuse me, can you be so kind as to get me that handsoap on the top shelf?", Answers: ["Sorry, I can't reach that.","Sorry, it's out of my reach. Let me get an employee.","Hang on, if I stand on the shelves I can get it.","Nope, I can't get it."], Answer: 1)]
+        
+        randomQueston()
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,6 +44,55 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func randomQueston() {
+        if Questions.count > 0 {
+            QNumber = random() % Questions.count
+            questionLabel.text = Questions[QNumber].Question
+            
+            AnswerNumber = Questions[QNumber].Answer
+            
+            for i in 0..<Buttons.count{
+                Buttons[i].setTitle(Questions[QNumber].Answers[i], forState: UIControlState.Normal)
+            }
+            
+            Questions.removeAtIndex(QNumber)
+        } else {
+            NSLog("Questions Finsished!")
+            NSLog("\(Score)")
+        }
+    }
 
+    @IBAction func btn1(sender: AnyObject) {
+        if AnswerNumber == 0 {
+            randomQueston()
+            Score += 1
+        } else {
+            NSLog("Question Was Answered Wrong!")
+        }
+    }
+    @IBAction func btn2(sender: AnyObject) {
+        if AnswerNumber == 1 {
+            randomQueston()
+            Score += 1
+        } else {
+            NSLog("Question Was Answered Wrong!")
+        }
+    }
+    @IBAction func btn3(sender: AnyObject) {
+        if AnswerNumber == 2 {
+            randomQueston()
+            Score += 1
+        } else {
+            NSLog("Question Was Answered Wrong!")
+        }
+    }
+    @IBAction func btn4(sender: AnyObject) {
+        if AnswerNumber == 3 {
+            randomQueston()
+            Score += 1
+        } else {
+            NSLog("Question Was Answered Wrong!")
+        }
+    }
 }
 
